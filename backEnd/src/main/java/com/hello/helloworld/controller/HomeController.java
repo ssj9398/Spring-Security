94524c599e94d3ad4609113f55c089d84178715d
alisyabob <sjs939883@gmail.com>
2021-10-30 11:03:13 +0900

    [Add] Spring Security 설정

--- backEnd/src/main/java/com/hello/helloworld/controller/HomeController.java
+++ backEnd/src/main/java/com/hello/helloworld/controller/HomeController.java
@@ -2,12 +2,38 @@ package com.hello.helloworld.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping({"/",""})
-    public String index(){
+    @GetMapping({"/", ""})
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료!";
     }
 }
